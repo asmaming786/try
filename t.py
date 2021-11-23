9984651da13482dbc9946ffb513008b04038b070
asmaming786 <asmaming786@gmail.com>
2021-11-23 21:31:30 +0800

    iiii

---
+++ t.py
@@ -0,0 +1,78 @@
+pppppppublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+ublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+ublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+ublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+ublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+ublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+ublic class java{
+
+}
+
+
+public main{
+
+
+
+}
+
+
